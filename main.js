99900031c7ee8cd3c5470f85842212e46b19814f
Lokuyow <94349922+Lokuyow@users.noreply.github.com>
2023-11-20 21:37:07 +0900

    second

--- main.js
+++ main.js
@@ -1,69 +1,67 @@
+// startDateの最大値を設定
+document.getElementById('startDate').max = new Date().toISOString().split("T")[0];
+
 document.getElementById('investmentForm').addEventListener('submit', function (event) {
     event.preventDefault();
-
     const startDate = document.getElementById('startDate').value;
     const monthlyAmount = document.getElementById('amount').value;
-
     fetchBitcoinData(startDate, monthlyAmount);
 });
 
 function fetchBitcoinData(startDate, monthlyAmount) {
-    const currentUnixTime = Math.floor(new Date().getTime() / 1000); // 現在のUNIX時間（秒単位）
-    const startDateFormat = new Date(startDate).toISOString().split('T')[0]; // 開始日の年月日形式
-    const apiUrl = `https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=jpy&from=${new Date(startDateFormat).getTime() / 1000}&to=${currentUnixTime}`;
+    const currentUnixTime = Math.floor(new Date().getTime() / 1000);
+    const startUnixTime = Math.floor(new Date(startDate).getTime() / 1000);
+    const apiUrl = `https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=jpy&from=${startUnixTime}&to=${currentUnixTime}`;
 
-    // コンソールにAPIのURLを出力
     console.log("API URL: ", apiUrl);
 
     fetch(apiUrl)
         .then(response => response.json())
-        .then(data => {
-            // startDateをdisplayResultsに渡す
-            displayResults(data, monthlyAmount, startDate);
-        })
-        .catch(error => {
-            console.error('Error fetching data: ', error);
-        });
+        .then(data => displayResults(data, monthlyAmount, startDate))
+        .catch(error => console.error('Error fetching data: ', error));
 }
 
 function displayResults(data, monthlyAmount, startDate) {
-    const prices = data.prices;
-    let totalInvestment = 0;
-    let totalBitcoinPurchased = 0; // 購入したビットコインの総量
-    let totalValue = 0;
-    let investmentCount = 0;
+    let totalInvestment = 0, totalBitcoinPurchased = 0, totalValue = 0, investmentCount = 0;
     const startDay = new Date(startDate).getDate();
-
     let lastInvestmentMonth = -1;
-    prices.forEach((price) => {
-        const priceDate = new Date(price[0]);
-        const year = priceDate.getFullYear();
-        const month = priceDate.getMonth();
-        const day = priceDate.getDate();
 
-        if (month !== lastInvestmentMonth) {
-            let investmentDay = Math.min(startDay, new Date(year, month + 1, 0).getDate());
-
-            if (day === investmentDay || (day > investmentDay && month !== lastInvestmentMonth)) {
-                lastInvestmentMonth = month;
-                const btcAmount = monthlyAmount / price[1]; // 購入したビットコインの量
-                totalInvestment += parseInt(monthlyAmount, 10);
-                totalBitcoinPurchased += btcAmount; // ビットコイン購入量を加算
-                totalValue += btcAmount * prices[prices.length - 1][1];
-                investmentCount++;
-            }
-        }
+    data.prices.forEach((price) => {
+        const [date, value] = price;
+        processInvestment(new Date(date), value);
     });
 
     const profit = totalValue - totalInvestment;
     const profitRate = (profit / totalInvestment) * 100;
+    const profitRateSign = profitRate > 0 ? "+" : "";
+    const roundedTotalValue = Math.round(totalValue);
 
-    document.getElementById('results').innerHTML = `
-        <h2>投資結果</h2>
-        <p>投資回数: ${investmentCount} 回</p>
-        <p>購入したビットコインの総量: ${totalBitcoinPurchased.toFixed(8)} BTC</p>
-        <p>総投資額: ${totalInvestment.toLocaleString('ja-JP')} 円</p>
-        <p>現在の評価額: ${totalValue.toLocaleString('ja-JP')} 円</p>
-        <p>利益率: ${profitRate.toFixed(2)}%</p>
-    `;
+    updateResultsDisplay(investmentCount, totalInvestment, roundedTotalValue, profitRateSign, profitRate, totalBitcoinPurchased);
+
+    function processInvestment(date, value) {
+        const year = date.getFullYear();
+        const month = date.getMonth();
+        const day = date.getDate();
+        const investmentDay = Math.min(startDay, new Date(year, month + 1, 0).getDate());
+
+        if (month !== lastInvestmentMonth && (day === investmentDay || day > investmentDay)) {
+            lastInvestmentMonth = month;
+            const btcAmount = monthlyAmount / value;
+            totalInvestment += parseInt(monthlyAmount, 10);
+            totalBitcoinPurchased += btcAmount;
+            totalValue += btcAmount * data.prices[data.prices.length - 1][1];
+            investmentCount++;
+        }
+    }
+
+    function updateResultsDisplay(count, investment, value, sign, rate, btc) {
+        document.getElementById('results').innerHTML = `
+            <h2 class="results-header">結果</h2>
+            <p class="results-text">購入回数：${count} 回</p>
+            <p class="results-text">総購入金額：${investment.toLocaleString('ja-JP')} 円</p>
+            <p class="results-important">現在の評価額：${value.toLocaleString('ja-JP')} 円</p>
+            <p class="results-important">利益率：${sign}${rate.toFixed(2)}%</p>
+            <p class="results-btc">保有BTC：${btc.toFixed(8)} BTC</p>
+        `;
+    }
 }
